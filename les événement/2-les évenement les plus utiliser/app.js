5219d413aefd1d86f9d5bef13868d633fa0b19b1
Fcordier7921 <fredericcordierpro6@gmail.com>
2021-09-28 09:32:49 +0200

    les évenement les plus utiliser en js

--- les événement/2-les évenement les plus utiliser/app.js
+++ les événement/2-les évenement les plus utiliser/app.js
@@ -1,6 +1,30 @@
 const btn = document.querySelector('button');
+const suvole = document.querySelector('.box');
+const inp = document.querySelector('input');
 
-btn.addEventListener('click', () => {
-    console.log('click');
-});
+btn.addEventListener('click', foo);
 //on utilise uniquement addEventListener pour écouter un événement
+function foo() {
+    console.log('click');
+    btn.removeEventListener('click', foo);
+}
+
+suvole.addEventListener('mouseenter', () => {
+    console.log("survolle");
+    suvole.style.backgroundColor = 'blue';
+});
+suvole.addEventListener('mouseout', () => {
+    console.log("fin de survolle");
+    suvole.style.backgroundColor = 'crimson';
+});
+document.addEventListener('keydown', (event) => {
+    console.log(event.key);
+});
+window.addEventListener('load', () => {
+    console.log(' tout a bien chargement');
+});
+
+inp.addEventListener('input', () => {
+    console.log("je suis entrain d'écrire dans l'input");
+    console.log(inp.value);
+});
