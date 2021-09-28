1ed2058b10d837fb7d1c52924de49513c6295b14
Fcordier7921 <fredericcordierpro6@gmail.com>
2021-09-28 11:01:02 +0200

    les porté des variables

---
+++ les régles js mémo/1- porté des variable/app.js
@@ -0,0 +1,29 @@
+let foo = "abc"; // variable globale de partout
+
+console.log(foo);
+
+function tutu() {
+    console.log(foo);
+}
+tutu();
+if (true) {
+    console.log(foo);
+}
+
+//variable dans une fonction l'accé rezste dans la fontion
+function faa() {
+    let bar = 3;
+    console.log(bar);
+    if (true) {
+        if (true) {
+            var lorem = " lorem"; // les var son funtion scopp on y  a accé dans tout la fonction ou tout les bloc
+            let reg = " reg"; // les let et const son accésible juste dans le block qui les contien
+            console.log(reg);
+        }
+    }
+    console.log(lorem);
+    console.log(reg);
+
+}
+faa();
+console.log(bar);
