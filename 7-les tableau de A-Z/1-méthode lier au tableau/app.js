d608e6890bc2facce1e8045d8570c800d6553a01
Fcordier7921 <fredericcordierpro6@gmail.com>
2021-10-04 09:25:21 +0200

    commentaire méthode tableau partie1

--- 7-les tableau de A-Z/1-méthode lier au tableau/app.js
+++ 7-les tableau de A-Z/1-méthode lier au tableau/app.js
@@ -1,11 +1,11 @@
 const tableau = ['a', 'b', 'c', 'd'];
 
-tableau.push('z');
-console.log(tableau);
-tableau.pop();
-console.log(tableau);
-tableau.shift();
-console.log(tableau);
-tableau.unshift('a', 'z');
-console.log(tableau);
-console.log(tableau.lastIndexOf('z'));
+tableau.push('z'); //rajouter un élément a la fin du tableau
+console.log(`push :`, tableau);
+tableau.pop(); //retire le dernier éléement d'un tableau
+console.log(`pop :`, tableau);
+tableau.shift(); //va enlever le prenier élement d"un tableau
+console.log(`shift :`, tableau);
+tableau.unshift('a', 'z'); //rajoute des élement au début
+console.log(`unshift :`, tableau);
+console.log(`lastIndexOf :`, tableau.lastIndexOf('z')); //donne l'index du tableau
