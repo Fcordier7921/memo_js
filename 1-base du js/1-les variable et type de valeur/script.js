0cf25658ee46a7067e54ed9a65e829563ce940ae
Fcordier7921 <fredericcordierpro6@gmail.com>
2021-10-06 10:49:48 +0200

    fin2

--- 1-base du js/1-les variable et type de valeur/script.js
+++ 1-base du js/1-les variable et type de valeur/script.js
@@ -2,7 +2,7 @@
 let prenom = "enzo";
 let nom; // undefined ne peut pas faiire la même chose pour un const, il faut définire un variable const
 const prenom2 = "lulu";
-const nomPosteFemelle; //kamelkaase 
+// const nomPosteFemelle; //kamelkaase 
 //-----------------------------------------------
 // les types de variable que l'on peut mettre en js
 
@@ -30,3 +30,18 @@ const myObj = { //on déclare les objet avec leur va leur a l'intrérieur
 };
 
 //Symbol
+
+// // trtansformet un chaine de caratére en chiffre
+
+let nub = parseInt('45'); //pour des entier
+console.log(`${nub} est un ${typeof nub}`);
+
+let nub2 = parseFloat('45.5325'); //pour des nombre avirgule
+console.log(`${nub2} est un ${typeof nub2}`);
+
+
+//convertire en chaine de caractére
+
+let nunerFree = 45;
+let tutu = nunerFree.toString();
+console.log(`${tutu} est un ${typeof tutu}`);
