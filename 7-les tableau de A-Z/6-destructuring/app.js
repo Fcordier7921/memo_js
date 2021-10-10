96cb00681704a306486b2cdfa087b52685e42931
Fcordier7921 <fredericcordierpro6@gmail.com>
2021-10-10 16:09:23 +0200

    rajout destutrucring2

--- 7-les tableau de A-Z/6-destructuring/app.js
+++ 7-les tableau de A-Z/6-destructuring/app.js
@@ -1,11 +1,11 @@
 let informations = ['superSayen', '25', 'homme'];
 
-// Sans décomposition
+// Sans décomposition si j'utilise cette méthode je dois passe par des variable
 // let pseudo = informations[0];
 // let age    = informations[1];
 // let sexe   = informations[2];
 
-// Avec décomposition
+// Avec décomposition les variable qui sont entre croché coresponde au valeur du tableau dans la même orde
 let [pseudo, age, sexe] = informations;
 
 console.log(pseudo);
